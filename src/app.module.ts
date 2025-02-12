20aeba18a1613732e04a9746bffd5c628b73b484
Davison Mota <davimotagnr@gmail.com>
2025-02-12 17:49:06 -0300

    feat: create Prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { VideosModule } from './videos/videos.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import * as Joi from 'joi';
         DATABASE_URL: Joi.string().required(),
       }),
     }),
+    DatabaseModule,
     VideosModule,
   ],
   controllers: [],
