08bea8fed0c0649b3791e0364dde8ea1c8352ea0
Davison Mota <davimotagnr@gmail.com>
2025-02-13 15:43:44 -0300

    test: ensure VideosService.create is called with correct data

--- src/videos/videos.controller.spec.ts
+++ src/videos/videos.controller.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VideosController } from './videos.controller';
 import { VideosService } from './videos.service';
+import { Readable } from 'stream';
+
+const mockServiceVideo = {
+  create: jest.fn(),
+};
 
 describe('VideosController', () => {
   let controller: VideosController;
@@ -8,13 +13,36 @@ describe('VideosController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VideosController],
-      providers: [VideosService],
+      providers: [{ provide: VideosService, useValue: mockServiceVideo }],
     }).compile();
 
     controller = module.get<VideosController>(VideosController);
   });
 
-  it.skip('should be defined', () => {
+  it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call VideoService.create with correct data', async () => {
+    const mockFile: Express.Multer.File = {
+      fieldname: 'file',
+      originalname: 'GravaÃ§Ã£o de tela de 2025-02-12 18-22-36.webm',
+      encoding: '7bit',
+      mimetype: 'video/webm',
+      destination: './uploadedFiles/videos',
+      filename: '9e43e7cb6b98e08d4975de7fc176baf8',
+      path: 'uploadedFiles/videos/9e43e7cb6b98e08d4975de7fc176baf8',
+      size: 403509,
+      buffer: Buffer.from('test video'),
+      stream: new Readable(),
+    };
+
+    await controller.addVideo(mockFile);
+
+    expect(mockServiceVideo.create).toHaveBeenCalledWith({
+      filename: mockFile.originalname,
+      path: mockFile.path,
+      mimetype: mockFile.mimetype,
+    });
   });
 });
